39d1fc6c4eaaf621a80bb4c3b7216deacf299020
Hector Flores <hecto932@gmail.com>
2018-12-22 17:50:00 -0600

    Implementacion de CRUD

---
+++ ecommerce/routes/api/products.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const productMocks = require('../../utils/mocks/products');
+
+router.get('/', function (req, res) {
+  const { query } = req;
+
+  res.status(200).json({
+    data: productMocks,
+    message: 'products listed'
+  })
+})
+
+router.get('/:productId', function (req, res) {
+  const { productId } = req.params;
+
+  res.status(200).json({
+    data: productMocks[0],
+    message: 'product retrieved'
+  })
+})
+
+router.post('/', function (req, res) {
+  res.status(201).json({
+    data: productMocks[0],
+    message: 'products listed'
+  })
+})
+
+router.put('/:productId', function (req, res) {
+  res.status(200).json({
+    data: productMocks,
+    message: 'products updated'
+  })
+})
+
+router.delete('/', function (req, res) {
+  res.status(200).json({
+    data: productMocks[0],
+    message: 'product deleted'
+  })
+})
+
+module.exports = router;
