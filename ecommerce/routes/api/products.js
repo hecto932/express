2e59110c853ad9cb5ec741e363335755163d54ef
Hector Flores <hecto932@gmail.com>
2018-12-26 00:22:26 -0600

    Que es joy y boom y como configurar

--- ecommerce/routes/api/products.js
+++ ecommerce/routes/api/products.js
@@ -1,66 +1,79 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const ProductsService = require("../../services/products");
 
 const productService = new ProductsService();
 
-router.get("/", async function(req, res, next) {
+const validation = require('../../utils/middlewares/validationHandler');
+
+const {
+  productIdSchema,
+  productTagSchema,
+  createProductSchema,
+  updateProductSchema
+} = require('../../utils/schemas/products');
+
+router.get('/', async function(req, res, next) {
   const { tags } = req.query;
 
-  console.log("req", req.query);
+  console.log('req', req.query);
 
   try {
-    throw new Error('This is an error from the API');
+    // throw new Error('This is an error from the API');
     const products = await productService.getProducts({ tags });
 
     res.status(200).json({
       data: products,
-      message: "products listed"
+      message: 'products listed'
     });
   } catch (err) {
     next(err);
   }
 });
 
-router.get("/:productId", async function(req, res, next) {
+router.get('/:productId', async function(req, res, next) {
   const { productId } = req.params;
 
-  console.log("req", req.params);
+  console.log('req', req.params);
 
   try {
     const product = await productService.getProduct({ productId });
 
     res.status(200).json({
       data: product,
-      message: "product retrieved"
+      message: 'product retrieved'
     });
   } catch (err) {
     next(err);
   }
 });
 
-router.post("/", async function(req, res, next) {
+router.post('/', validation(createProductSchema), async function(req, res, next) {
   const { body: product } = req;
 
-  console.log("req", req.body);
+  console.log('req', req.body);
 
   try {
     const createdProduct = await productService.createProduct({ product });
 
     res.status(201).json({
       data: createdProduct,
-      message: "product created"
+      message: 'product created'
     });
   } catch (err) {
     next(err);
   }
 });
 
-router.put("/:productId", async function(req, res, next) {
+router.put(
+  '/:productId',
+  validation({ productId: productIdSchema }, 'params'),
+  validation(updateProductSchema),
+  async function(req, res, next) {
   const { productId } = req.params;
   const { body: product } = req;
 
-  console.log("req", req.params, req.body);
+  console.log('req', req.params, req.body);
 
   try {
     const updatedProduct = await productService.updateProduct({
@@ -69,24 +82,24 @@ router.put("/:productId", async function(req, res, next) {
     });
     res.status(200).json({
       data: updatedProduct,
-      message: "product updated"
+      message: 'product updated'
     });
   } catch (err) {
     next(err);
   }
 });
 
-router.delete("/:productId", async function(req, res, next) {
+router.delete('/:productId', async function(req, res, next) {
   const { productId } = req.params;
 
-  console.log("req", req.params);
+  console.log('req', req.params);
 
   try {
     const deletedProduct = await productService.deleteProduct({ productId });
 
     res.status(200).json({
       data: deletedProduct,
-      message: "product deleted"
+      message: 'product deleted'
     });
   } catch (err) {
     next(err);
