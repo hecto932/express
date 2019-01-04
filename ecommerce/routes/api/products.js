2c950bdc5a5ca75287be76e2b06d8a635130b515
Hector Flores <hecto932@gmail.com>
2019-01-04 08:32:41 -0600

    Agregando cache y helmet

--- ecommerce/routes/api/products.js
+++ ecommerce/routes/api/products.js
@@ -10,6 +10,9 @@ const {
   updateProductSchema
 } = require("../../utils/schemas/products");
 
+const cacheResponse = require('../../utils/cacheResponse');
+const { FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS } = require('../../utils/time');
+
 // JWT strategy
 require("../../utils/auth/strategies/jwt");
 
@@ -20,6 +23,7 @@ function productsApi(app) {
   const productService = new ProductsService();
 
   router.get("/", async function(req, res, next) {
+    cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
     const { tags } = req.query;
 
     try {
@@ -35,6 +39,7 @@ function productsApi(app) {
   });
 
   router.get("/:productId", async function(req, res, next) {
+    cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
     const { productId } = req.params;
 
     debug("req", req.params);
